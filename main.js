ed67785a1eed36df841023083afbb0657b1c89c7
Arm Supakorn <arm.supakorn@gmail.com>
2021-07-17 22:23:08 +0700

    Add basic start template

---
+++ main.js
@@ -0,0 +1,19 @@
+function addItemToCart() {
+    // name price amount
+    console.log("add item");
+}
+
+function deleteItem() {
+    console.log("delete this item");
+}
+
+function showCart() {
+    console.log("show items in cart");
+}
+
+function main() {
+    const addItemBtn = document.getElementById("add-item-btn");
+    addItemBtn.addEventListener("click", addItem);
+}
+
+main();
